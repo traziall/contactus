323e591db2a5ca8ffeb4fbecd225f8f3a5917beb
Apocalipsis117 <74636100+Apock-graficos@users.noreply.github.com>
2025-09-11 11:13:40 -0500

    queso

--- assets/vue/index/components/tz-contact.js
+++ assets/vue/index/components/tz-contact.js
@@ -1,4 +1,77 @@
 export const tzContact = {
+    data() {
+        return {
+            items: [
+                {
+                    group: "groupA",
+                    children: [
+                        {
+                            uuid: '09297a11-07fb-4c4e-940c-45d273193d77',
+                            icon: 'mi-business_center', // Contratarnos
+                            label: 'Contratarnos',
+                            color: 'text-red-400',
+                        },
+                        {
+                            uuid: '81e2fece-436e-4041-a399-1a8d5da14909',
+                            icon: 'mi-web', // App web
+                            label: 'app web',
+                            color: 'text-red-400',
+                        },
+                        {
+                            uuid: '254179b3-5a7d-43c0-8211-5e9b0d36c105',
+                            icon: 'mi-animation', // Animación gráfica
+                            label: 'Animacion grafica',
+                            color: 'text-red-400',
+                        },
+                        {
+                            uuid: 'f2765f5d-3e86-4798-af52-6c2267b40257',
+                            icon: 'mi-storage', // Back-end
+                            label: 'Back-end',
+                            color: 'text-red-400',
+                        },
+                        {
+                            uuid: '8a2f9083-eb04-421c-94b0-b2407e17c7ce',
+                            icon: 'mi-design_services', // Diseño UX UI
+                            label: 'Diseño UX UI',
+                            color: 'text-red-400',
+                        },
+                        {
+                            uuid: '9ff50cca-a4e0-4819-880b-afefaadfed8e',
+                            icon: 'mi-desktop_windows', // App desktop
+                            label: 'App desktop',
+                            color: 'text-red-400',
+                        },
+                        {
+                            uuid: '5eba8af9-b877-4832-b72e-570d6525ac4a',
+                            icon: 'mi-smartphone', // App móvil
+                            label: 'App movil',
+                            color: 'text-red-400',
+                        },
+                        {
+                            uuid: '7c827907-d466-4361-90df-574623a4379c',
+                            icon: 'mi-brush', // Ilustraciones
+                            label: 'Ilustraciones',
+                            color: 'text-red-400',
+                        },
+                        {
+                            uuid: '40cdf557-360b-4fe1-8385-7597532c1802',
+                            icon: 'mi-movie_edit', // Video edición (Material Symbols)
+                            label: 'Video edicion',
+                            color: 'text-red-400',
+                        },
+                    ],
+                },
+                {
+                    group: "groupB",
+                    children: [],
+                },
+            ],
+            itemLookup: {}, // para mapear uuid → objeto
+        };
+    },
+    mounted() {
+        this.looup()
+    },
     template: /* html */`
         <section class="block">
             <div class="mx-auto max-w-5xl bg-(--gray-850) border border-gray-800 p-10 -mt-16 rounded-t-lg">
@@ -15,7 +88,7 @@ export const tzContact = {
                             :data-uuid="item.uuid">
                                 <div class="text-center">
                                     <figure class="tw-icono bg-gray-700/40 shadow-md m-0 p-0">
-                                        <i class="mi" :class="item.icon"></i>
+                                        <i class="mi" :class="[item.icon, item.color]"></i>
                                     </figure>
                                     <p class="text-[0.8rem] text-gray-400">{{ item.label }}</p>
                                 </div>
@@ -34,7 +107,7 @@ export const tzContact = {
                             :data-uuid="item.uuid">
                                 <div class="text-center">
                                     <figure class="tw-icono bg-gray-700/40 shadow-md m-0 p-0">
-                                        <i class="mi" :class="item.icon"></i>
+                                        <i class="mi" :class="[item.icon, item.color]"></i>
                                     </figure>
                                     <p class="text-[0.8rem] text-gray-400">{{ item.label }}</p>
                                 </div>
@@ -46,88 +119,6 @@ export const tzContact = {
             </div>
         </section>
     `,
-    data() {
-        return {
-            items: [
-                {
-                    group: "groupA",
-                    children: [
-                        {
-                            uuid: '09297a11-07fb-4c4e-940c-45d273193d77',
-                            icon: 'mi-business_center', // Contratarnos
-                            label: 'Contratarnos'
-                        },
-                        {
-                            uuid: '81e2fece-436e-4041-a399-1a8d5da14909',
-                            icon: 'mi-web', // App web
-                            label: 'app web'
-                        },
-                        {
-                            uuid: '254179b3-5a7d-43c0-8211-5e9b0d36c105',
-                            icon: 'mi-animation', // Animación gráfica
-                            label: 'Animacion grafica'
-                        },
-                        {
-                            uuid: 'f2765f5d-3e86-4798-af52-6c2267b40257',
-                            icon: 'mi-storage', // Back-end
-                            label: 'Back-end'
-                        },
-                        {
-                            uuid: '8a2f9083-eb04-421c-94b0-b2407e17c7ce',
-                            icon: 'mi-design_services', // Diseño UX UI
-                            label: 'Diseño UX UI'
-                        },
-                        {
-                            uuid: '9ff50cca-a4e0-4819-880b-afefaadfed8e',
-                            icon: 'mi-desktop_windows', // App desktop
-                            label: 'App desktop'
-                        },
-                        {
-                            uuid: '5eba8af9-b877-4832-b72e-570d6525ac4a',
-                            icon: 'mi-smartphone', // App móvil
-                            label: 'App movil'
-                        },
-                        {
-                            uuid: '7c827907-d466-4361-90df-574623a4379c',
-                            icon: 'mi-brush', // Ilustraciones
-                            label: 'Ilustraciones'
-                        },
-                        {
-                            uuid: '40cdf557-360b-4fe1-8385-7597532c1802',
-                            icon: 'mi-movie_edit', // Video edición (Material Symbols)
-                            label: 'Video edicion'
-                        },
-                    ],
-                },
-                {
-                    group: "groupB",
-                    children: [],
-                },
-            ],
-            itemLookup: {}, // para mapear uuid → objeto
-        };
-    },
-
-    mounted() {
-        // inicializar lookup
-        this.items.forEach((group) => {
-            group.children.forEach((child) => {
-                this.itemLookup[child.uuid] = child;
-            });
-        });
-
-        // crear Sortable para cada grupo
-        this.items.forEach((group) => {
-            const el = document.getElementById(group.group);
-            Sortable.create(el, {
-                group: "shared",
-                animation: 150,
-                onAdd: this.updateItemsArray,
-                onRemove: this.updateItemsArray,
-                onUpdate: this.updateItemsArray,
-            });
-        });
-    },
 
     methods: {
         updateItemsArray() {
@@ -141,5 +132,25 @@ export const tzContact = {
 
             // console.log(JSON.stringify(this.items, null, 2));
         },
+        looup() {
+            // inicializar lookup
+            this.items.forEach((group) => {
+                group.children.forEach((child) => {
+                    this.itemLookup[child.uuid] = child;
+                });
+            });
+
+            // crear Sortable para cada grupo
+            this.items.forEach((group) => {
+                const el = document.getElementById(group.group);
+                Sortable.create(el, {
+                    group: "shared",
+                    animation: 150,
+                    onAdd: this.updateItemsArray,
+                    onRemove: this.updateItemsArray,
+                    onUpdate: this.updateItemsArray,
+                });
+            });
+        }
     },
 };
